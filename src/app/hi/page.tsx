1a7fcfe3162e13dfaf3786662988d322f588397c
Francois Best <github@francoisbest.com>
2024-05-30 21:48:46 +0200

    feat: Add Hi page

---
+++ src/app/hi/page.tsx
@@ -0,0 +1,37 @@
+export default function Page() {
+  return (
+    <main>
+      <h1>François Best</h1>
+      <p>Find me on:</p>
+      <ul>
+        <li>
+          <a href="https://x.com/fortysevenfx">X / Twitter</a>
+        </li>
+        <li>
+          <a href="https://mamot.fr/@Franky47">Mastodon</a>
+        </li>
+        <li>
+          <a href="https://github.com/franky47">GitHub</a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/francoisbest">LinkedIn</a>
+        </li>
+      </ul>
+      <p>Some of the things I'm working on:</p>
+      <ul>
+        <li>
+          <a href="https://nuqs.47ng.com">nuqs</a>, a type-safe search params
+          state manager for Next.js
+        </li>
+        <li>
+          <a href="https://chiffre.io">Chiffre.io</a>, an end-to-end encrypted
+          web analytics SaaS
+        </li>
+        <li>
+          And many other{' '}
+          <a href="https://github.com/47ng">open-source libraries</a>
+        </li>
+      </ul>
+    </main>
+  )
+}
